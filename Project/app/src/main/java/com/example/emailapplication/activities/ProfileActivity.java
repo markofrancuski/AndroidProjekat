2e3e4c883f75ed53e0da298150a81418dd76d281
Marko Francuski <francmarko@yahoo.com>
2020-07-20 23:18:22 +0200

    Fix app crash upon entering Profile Actvitiy

--- Project/app/src/main/java/com/example/emailapplication/activities/ProfileActivity.java
+++ Project/app/src/main/java/com/example/emailapplication/activities/ProfileActivity.java
@@ -1,17 +1,37 @@
 package com.example.emailapplication.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.emailapplication.R;
 
 public class ProfileActivity  extends AppCompatActivity {
 
+    private Button btn_Logout;
+
+    private void Logout(){
+
+
+        // Logout user.
+
+        // Return him to the login Activity.
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
+
+        btn_Logout = (Button) findViewById(R.id.btn_logout);
+        btn_Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Logout();
+            }
+        });
+
     }
 
     @Override
